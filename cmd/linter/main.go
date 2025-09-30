57f7f8baf922d6dc4fe199c4b6605882314caef9
bsten-tyk <221599321+bsten-tyk@users.noreply.github.com>
2025-09-30 13:42:51 +0300

    add DoD check status valid
    Signed-off-by: bsten-tyk <221599321+bsten-tyk@users.noreply.github.com>

--- cmd/linter/main.go
+++ cmd/linter/main.go
@@ -23,6 +23,7 @@ var defaultAcceptedStatuses = map[string]bool{
 	"in dev":         true,
 	"in code review": true,
 	"ready for dev":  true,
+	"DoD Check":      true,
 }
 
 // loadConfig loads and validates all environment configuration
